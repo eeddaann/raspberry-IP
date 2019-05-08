fe52f9f3fa40ef89394eef3fc26e4814e0969c16
eeddaann <edan100@gmail.com>
2019-05-08 18:27:20 +0300

    Update send_ip.sh

--- send_ip.sh
+++ send_ip.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-
-TOKEN=<TOKEN>
-CHAT_ID=<CHAT_ID>
+### BEGIN INIT INFO
+# Provides:          send_ip.sh
+# Required-Start:    $all
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Sends ip to telegram
+# Description:       A simple script that send your private ip to telegram
+### END INIT INFO
+TOKEN='< TOKEN >'
+CHAT_ID='< CHAT_ID >'
 MESSAGE=$(hostname -I | awk '{print $1}')
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
